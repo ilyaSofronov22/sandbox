178a7809828e5f712a8f627f162fc6a161b9572f
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-08-28 21:30:26 +0400

    First tasks

---
+++ freeCodeCamp/4.js
@@ -0,0 +1,8 @@
+//Assign the contents of a to variable b.
+
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
---
+++ freeCodeCamp/6.js
@@ -0,0 +1,7 @@
+/*Create two new string variables: 
+myFirstName and myLastName 
+and assign them the values of your first and last name, respectively.
+*/
+
+var myFirstName = 'Ilya';
+var myLastName = 'Sofronov';
