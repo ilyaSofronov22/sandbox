06d3b4c8263ab2f54c101d7be8d42c210f1d6530
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-09-01 21:42:23 +0400

    Undefined Value returned from a Function - 1

---
+++ freeCodeCamp/Understanding Undefined Value returned from a Function.js
@@ -0,0 +1,21 @@
+//Instructions
+//Create a function addFive without any arguments. 
+//This function adds 5 to the sum variable, but its returned value is undefined.
+
+//Solution
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
