66c8412b202c76a30f09ac0247e2a6771e67150d
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-09-02 21:36:49 +0400

    Assignment with a Returned Value - 1

---
+++ freeCodeCamp/Assignment with a Returned Value.js
@@ -0,0 +1,13 @@
+//Instructions
+//Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+//Solution
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed =  processArg(7);
