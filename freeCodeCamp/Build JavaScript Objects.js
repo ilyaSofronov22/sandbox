adb37d1bb021e20a76999463ce219f58be5f2919
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-09-03 23:50:36 +0400

    Build JavaScript Objects - 1

---
+++ freeCodeCamp/Build JavaScript Objects.js
@@ -0,0 +1,16 @@
+//Instructions
+//Make an object that represents a dog called myDog which contains the properties name (a string), 
+//legs, tails and friends.
+//You can set these object properties to whatever values you want, as long as name is a string, 
+//legs and tails are numbers, and friends is an array.
+
+//Solution
+const myDog = {
+    // Only change code below this line
+    name: 'Reks',
+    legs: 4,
+    tails: 1,
+    friends: ['Scotty', 'John']
+  
+    // Only change code above this line
+};
