f135fafefcc9ba5982808e1f20afad7819a31400
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-09-04 21:46:00 +0400

    Count Backwards With a For Loop - 1

---
+++ freeCodeCamp/Count Backwards With a For Loop.js
@@ -0,0 +1,11 @@
+//Instructions
+//Push the odd numbers from 9 through 1 to myArray using a for loop
+
+//Solution
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -=2) {
+  myArray.push(i);
+}
