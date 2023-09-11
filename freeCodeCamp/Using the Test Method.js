a59e341e71a98dc04628ecb9f607dcc17189693e
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-09-11 21:43:42 +0400

    Using the Test Method - 1

---
+++ freeCodeCamp/Using the Test Method.js
@@ -0,0 +1,7 @@
+//Instructions
+//Apply the regex myRegex on the string myString using the .test() method.
+
+//Solution
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
