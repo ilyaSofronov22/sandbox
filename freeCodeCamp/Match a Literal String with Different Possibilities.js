a96dc402c7ee01d346a3e3d02f13fbb980098e62
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-09-11 21:43:20 +0400

    Match a Literal String - 1

---
+++ freeCodeCamp/Match a Literal String with Different Possibilities.js
@@ -0,0 +1,7 @@
+//Instructions
+//Complete the regex petRegex to match the pets dog, cat, bird, or fish.
+
+//Solution
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
