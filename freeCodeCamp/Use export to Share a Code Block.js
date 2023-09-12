f5a4eadf4dd3a69cc977f795134a96a2c00c9afd
ilyaSofronov22 <i.sofronov.22@yandex.ru>
2023-09-12 22:01:00 +0400

    Use export to Share a Code Block - 1

---
+++ freeCodeCamp/Use export to Share a Code Block.js
@@ -0,0 +1,13 @@
+//Instructions
+//There are two string-related functions in the editor. Export both of them using the method of your choice.
+
+//Solution
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
+
+export {uppercaseString, lowercaseString};
